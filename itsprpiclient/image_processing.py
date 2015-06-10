f7c28b4af0afe1d2a4fbb3c1bda0da627855ba48
parthk-git <parthkothari811@gmail.com>
2015-06-10 19:03:00 +0530

    impro update

--- itsprpiclient/image_processing.py
+++ itsprpiclient/image_processing.py
@@ -1,68 +1,79 @@
 import cv2
 import numpy as np
 
+##number of squares in chessboard
+pk = 8
+
+##min value for canny-threshold
+min_value = 50
+
+##max value for canny-threshold
+max_value = 100
+
 ## an array having the x and y coordinate of the cornersof the chessboard
-corners = [[[0 for a in range(2)] for a in range(9)]for a in range(9)]
+corners = [[[0 for a in range(2)] for a in range(pk + 1)]for a in range(pk + 1)]
 
 ##an array having the x and y coordinates of the centres of the chessboard
-centres = [[[0 for a in range(2)] for a in range(8)]for a in range(8)]
+centres = [[[0 for a in range(2)] for a in range(pk)]for a in range(pk)]
 
 ##an array storing the structure of the board after the move is played
-piece_presence = [[[0 for a in range(2)] for a in range(8)]for a in range(8)]
+piece_presence = [[[0 for a in range(2)] for a in range(pk)]for a in range(pk)]
 
 ##an array storing the structure of the board before the move is played
-prev_presence  = [[[0 for a in range(2)] for a in range(8)]for a in range(8)]
+prev_presence  = [[[0 for a in range(2)] for a in range(pk)]for a in range(pk)]
 
 ##determines whos turn it is
 turn = 1
 
-## determines threshold for checking color of piece 
-threshold = 30000
+## determines threshold for checking color of piece
+check_color_threshold = 1000
+
+##Dimension of square whose region to be checked for color detection
+color_square = 10 
 
 ##function sets the initial conditions at the start of play
 def calibrate():
-
-    ####click photo left ##check when pressed
+####click photo left ##check when pressed
     img = np.copy(frame)
-##    img = cv2.imread( 'emptyboard.jpg', 1 )
-    patternsize = ( 9,9 )
+##img = cv2.imread( 'emptyboard.jpg', 1 )
+    patternsize = ( pk + 1, pk + 1 )
     corn = cv2.findChessboardCorners( img, patternsize )
 
     ##calibrating the corners array
-    for i in range( 0 , 9 ):
-        for j in range( 0 , 9 ):
-            corners[ i ][ j ][ 0 ] = corn[ 1 ][ 9*i+j, 0, 0 ]
-            corners[ i ][ j ][ 1 ] = corn[ 1 ][ 9*i+j, 0, 1 ]
+    for i in range( 0 , pk + 1 ):
+        for j in range( 0 , pk + 1 ):
+            corners[ i ][ j ][ 0 ] = corn[ 1 ][ (pk + 1)*i+j, 0, 0 ]
+            corners[ i ][ j ][ 1 ] = corn[ 1 ][ (pk + 1)*i+j, 0, 1 ]
             print corners[i][j]
 
     ##calibrating the centres array        
-    for i in range( 0 , 8 ):
-        for j in range( 0 , 8 ):
+    for i in range( 0 , pk ):
+        for j in range( 0 , pk ):
             centres[ i ][ j ][ 0 ] = (corners[ i ][ j ][ 0 ] + corners[ i+1 ][ j ][ 0 ] + corners[ i ][ j+1 ][ 0 ] + corners[ i+1 ][ j+1 ][ 0 ])/4
             centres[ i ][ j ][ 1 ] = (corners[ i ][ j ][ 1 ] + corners[ i+1 ][ j ][ 1 ] + corners[ i ][ j+1 ][ 1 ] + corners[ i+1 ][ j+1 ][ 1 ])/4
 
     ##setting up initial structure of pieces 
     for i in range( 0 , 2 ):
-        for j in range( 0 , 8 ):
+        for j in range( 0 , pk ):
             prev_presence[i][j] = 1
-            prev_presence[7-i][j] = -1
+            prev_presence[pk - 1 - i][j] = -1
     return
 
 ##checking whether color of piece is black or white 
 def check_color( r , c , img ):
     sumyy = 0
-    for i in range( centres[r][c][0] - 20 , centres[r][c][0] + 20):
-        for j in range( centres[r][c][1] - 20 , centres[r][c][1] + 20):
+    for i in range( centres[r][c][0] - color_square , centres[r][c][0] + color_square):
+        for j in range( centres[r][c][1] - color_square , centres[r][c][1] + color_square):
            sumyy = sumyy + img.item(i,j)
-    if sumyy < threshold:
+    if sumyy < check_color_threshold:
         return -1
     else:
         return 1
 
 ##checking whether piece is present or not in a square
 def check_presence( image, board  ):
-    for row in range( 0 , 8 ):
-        for column in range( 0 , 8 ):
+    for row in range( 0 , pk ):
+        for column in range( 0 , pk ):
             roi = image[(corners[row][column][1]+1):(corners[row+1][column+1][1]-1),(corners[row][column][0]+1):(corners[row+1][column+1][0]-1)]
             count = cv2.countNonZero(roi)
             if count > 700:
@@ -72,10 +83,10 @@ def check_presence( image, board  ):
     return
 
 ## comparing board positions before and after move is played and determining the move
-def find_move ():
+def find_move():
     counter = 0
-    for i in range( 0 , 8 ):
-        for j in range(0 , 8):
+    for i in range( 0 , pk ):
+        for j in range(0 , pk ):
             if not previous_presence[i][j] == piece_presence[i][j]:
                 counter++
                 if piece_presence[i][j] == turn : 
@@ -98,10 +109,10 @@ def find_move ():
                 b = [ 0 , 4 , 0 , 6 ]
                 
         else : ## blacks turn
-            if not previous_presence[7][0] == piece_presence[7][0]:
-                b = [ 7 , 4 , 7 , 2 ]
+            if not previous_presence[pk - 1][0] == piece_presence[pk - 1][0]:
+                b = [ pk - 1 , 4 , pk - 1 , 2 ]
             else :
-                b = [ 7 , 4 , 7 , 6 ]
+                b = [ pk - 1 , 4 , pk - 1 , 6 ]
 
     ## en - passent
     else :
@@ -129,10 +140,10 @@ def find_move ():
     
 
 ## updates after determining whether mov is legal or not
-def update ( ans ):
+def update( ans ):
     if ans == True:
-        for i in range( 0 , 8 ):
-            for j in range(0 , 8):
+        for i in range( 0 , pk ):
+            for j in range(0 , pk):
                 previous_presence[i][j] = piece_presence[i][j]
         turn = -turn
 
@@ -147,7 +158,7 @@ while 1: ##End game condition
     ##when key pressed
     ##board   = cv2.imread( 'board.jpg', 0 )
     board = np.copy(frame)
-    board_canny = cv2.Canny ( board , 11 , 90 )
+    board_canny = cv2.Canny ( board , min_value , max_value )
     check_presence( board_canny , board )
     find_move(turn)
 
