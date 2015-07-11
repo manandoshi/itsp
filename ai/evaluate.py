33c3acc85a3e4e7e48be868c19cfec2b1279864c
Harsh Sheth <harsheth96@gmail.com>
2015-07-11 07:41:53 +0530

    Add function evaluate

---
+++ ai/evaluate.py
@@ -0,0 +1,18 @@
+def evaluate(board):
+    whiteSum = 0
+    BlackSum = 0
+    for row in range(8):
+        for column in range(8):
+            piece = board[row][column]
+            if piece != 0:
+                if piece.color == 0:
+                    whiteSum += piece.value + piece.pointsTable[7-row][column]
+                else:
+                    assert piece.color == 1
+                    blackSum += piece.value + piece.pointsTable[row][column]
+    return blackSum - whiteSum                    
+
+
+
+
+
